5ffef932c1f7c215231d5fdde6d9e059ec96a0d5
fahaph <F_Phonwen@hotmail.com>
2025-12-27 00:22:18 +0700

    .

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const config = require("./config/config");
 
 app.listen(config.port, () => {
   console.log(`Server is running on http://localhost:${config.port}`);
-  console.log(`Health Check: http://localhost:${config.port}/api/v1/health`);
+  console.log(`Health Check eiei: http://localhost:${config.port}/api/v1/health`);
   console.log(`Gold Price: http://localhost:${config.port}/api/v1/gold-price`);
 });
